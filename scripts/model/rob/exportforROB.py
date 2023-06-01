54ff0190d29aaef94f2beadea843d2710f8ee91c
samyooole <kye_xuan@hotmail.com>
2023-06-01 11:13:31 +0800

    'haha'

--- scripts/model/rob/exportforROB.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('core_excels/totaPlus_MandatoryPredict.csv')
-text=df.text.astype('str')
-writePath = 'scripts/model/rob/tolabel.txt'
-text.to_csv(writePath, header=None, index=None, sep=' ', mode='a')
